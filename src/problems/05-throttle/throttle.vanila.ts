ecae9e39677578a8282d2c90a1297090db1f0c9f
Hareesh108 <hareeshbhittam@gmail.com>
2026-08-23 22:33:26 +0530

    Prepare for UI Interview V2 - throttle

--- src/problems/05-throttle/throttle.vanila.ts
+++ src/problems/05-throttle/throttle.vanila.ts
@@ -1,13 +1,26 @@
 // bun test src/problems/05-throttle/test/throttle.test.ts
 
-export function throttle() {
+export function throttle(fun: any, delay: number) {
+
+    let lastTime: any = null
+
+    return function inner(this: unknown, ...arg: any) {
+
+        const now = Date.now()
+
+        if (now - lastTime >= delay) {
+            fun.apply(this, arg)
+            lastTime = now
+        }
+
+    }
 
 }
 // --- Examples ---
 // Uncomment to test your implementation:
 
-// const log = throttle((msg: string) => console.log(msg), 300)
-// log('a')  // fires immediately → "a"
-// log('b')  // ignored (within 300ms)
-// log('c')  // ignored (within 300ms)
-// setTimeout(() => log('d'), 400)  // fires → "d" (300ms passed)
+const log = throttle((msg: string) => console.log(msg), 300)
+log('a')  // fires immediately → "a"
+log('b')  // ignored (within 300ms)
+log('c')  // ignored (within 300ms)
+setTimeout(() => log('d'), 400)  // fires → "d" (300ms passed)
