21c782e8a36e5661d2899d2b6f654a21fc58a049
Hareesh108 <hareeshbhittam@gmail.com>
2026-08-23 17:26:47 +0530

    Prepare for UI Interview V2 - debounce

--- src/problems/02-debounce/debounce.vanila.ts
+++ src/problems/02-debounce/debounce.vanila.ts
@@ -1,13 +1,29 @@
 // bun test src/problems/02-debounce/test/debounce.test.ts
 
-export function debounce() {
+export function debounce(func: any, delay: number) {
 
+    let time: any = null
+
+    return function inner(this: any, ...arg: any[]) {
+
+        if (time) {
+            clearTimeout(time)
+        }
+
+        time = setTimeout(() => {
+
+            func.apply(this, arg)
+
+        }, delay)
+
+    }
 }
+
 
 // --- Examples ---
 // Uncomment to test your implementation:
 
-// const log = debounce((msg: string) => console.log(msg), 300)
-// log('a')  // cancelled by next call
-// log('b')  // cancelled by next call
-// log('c')  // only this one fires after 300ms → "c"
+const log = debounce((msg: string) => console.log(msg), 300)
+log('a')  // cancelled by next call
+log('b')  // cancelled by next call
+log('c')  // only this one fires after 300ms → "c"
